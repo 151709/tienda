c2edb101de56c358810f7242c3a33f75439a9bc2
151709 <123423242+151709@users.noreply.github.com>
2023-05-08 11:31:02 -0600

    Add files via upload

---
+++ tienda/src/main/java/com/latam/alura/tienda/dao/ClienteDao.java
@@ -0,0 +1,49 @@
+package com.latam.alura.tienda.dao;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import com.latam.alura.tienda.modelo.Cliente;
+import com.latam.alura.tienda.modelo.Pedido;
+import com.latam.alura.tienda.modelo.Producto;
+
+public class ClienteDao {
+	
+	private EntityManager em;
+
+	public ClienteDao(EntityManager em) {
+		this.em = em;
+	}
+	
+	public void guardar(Cliente cliente) {
+		this.em.persist(cliente);
+	}
+	
+	public Cliente consultaPorId(Long id) {
+		return em.find(Cliente.class, id);
+	}
+	
+	public List<Cliente> consultarTodos(){
+		String jqpl= "SELECT P FROM Cliente AS P";
+		return em.createQuery(jqpl, Cliente.class).getResultList();
+	}
+	
+	public List<Cliente> consultarPorNombre(String nombre){
+		String jpql = " SELECT P FROM Cliente AS P WHERE P.nombre=: nombre";
+		return em.createQuery(jpql).setParameter("nombre", nombre).getResultList();	
+	}
+	
+	public List<Cliente> consultarPorNombreDeCategoria(String nombre){
+		String jpql = " SELECT p FROM Cliente AS p WHERE p.categoria.nombre=:nombre";
+		return em.createQuery(jpql).setParameter("nombre", nombre).getResultList();
+	}
+	
+	public BigDecimal consultarPrecioPorNombreDeProducto(String nombre) {
+		String jpql = " SELECT P.precio FROM Cliente AS P WHERE P.nombre=:nombre";
+		return em.createQuery(jpql, BigDecimal.class).setParameter("nombre", nombre).getSingleResult();
+	}
+	
+
+}
---
+++ tienda/src/main/java/com/latam/alura/tienda/modelo/Producto.java
@@ -0,0 +1,80 @@
+package com.latam.alura.tienda.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@SuppressWarnings("all")
+@Entity
+@Table(name="productos")
+@NamedQuery(name="Producto.consultaDePrecio", query=" SELECT P.precio FROM Producto AS P WHERE P.nombre=:nombre")
+public class Producto {
+	
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+    private LocalDate fechaDeRegistro = LocalDate.now();
+   
+    @ManyToOne
+    private Categoria categoria;
+
+    public Producto() {
+	}
+
+	public Producto(String nombre, String descripcion, BigDecimal precio, Categoria categoria) {
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.categoria = categoria;
+	}
+
+	public Long getId() {    
+    	return id;
+    	}
+
+    public void setId(Long id) {
+    	this.id = id;
+    	}
+
+    public String getNombre() {    
+    	return nombre;
+    	}
+
+    public void setNombre(String nombre) {
+    	this.nombre = nombre;
+    	}
+
+    public String getDescripcion() {
+    	return descripcion;
+    	}
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+}
+
+    public BigDecimal getPrecio() {
+    	return precio;
+    	}
+
+    public void setPrecio(BigDecimal precio) {
+    	this.precio = precio;
+    	}
+
+}
